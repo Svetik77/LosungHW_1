310cc17779ce7d155598b2242ab5b1730b5d5b51
Svetik77 <rimma34@gmail.com>
2017-04-05 22:55:20 +0200

    refactor 1

--- LosungHW_1/src/main/java/ru/javawebinar/topjava/repository/mock/InMemoryUserRepositoryImpl.java
+++ LosungHW_1/src/main/java/ru/javawebinar/topjava/repository/mock/InMemoryUserRepositoryImpl.java
@@ -3,37 +3,54 @@ package ru.javawebinar.topjava.repository.mock;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
+
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Repository
-public class MockUserRepositoryImpl implements UserRepository {
-    private static final Logger LOG = LoggerFactory.getLogger(MockUserRepositoryImpl.class);
+public class InMemoryUserRepositoryImpl implements UserRepository {
+    private static final Logger LOG = LoggerFactory.getLogger(InMemoryUserRepositoryImpl.class);
 
+    private Map<Integer, User> repository = new ConcurrentHashMap<Integer, User>();
+    private AtomicInteger counter = new AtomicInteger(0);
+    
+    
     @Override
     public boolean delete(int id) {
         LOG.info("delete " + id);
+        repository.remove(id);
         return true;
     }
 
     @Override
     public User save(User user) {
-        LOG.info("save " + user);
+    	if(user.isNew()) {
+    		user.setId(counter.incrementAndGet());
+    	}
+    	repository.put(user.getId(), user);
+         LOG.info("save " + user);
         return user;
     }
 
     @Override
     public User get(int id) {
         LOG.info("get " + id);
-        return null;
+        return repository.get(id);
     }
 
     @Override
     public List<User> getAll() {
         LOG.info("getAll");
+        
+        //  sort by the name list of users // TODO 
+//        return repository.values();
         return Collections.emptyList();
     }
 
